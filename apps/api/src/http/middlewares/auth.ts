f663470c3edfa6b9bc75cf6037ff7ce3cc7b1b52
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-09-08 16:53:16 -0300

    feat(back): get organization membership

--- apps/api/src/http/middlewares/auth.ts
+++ apps/api/src/http/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import { fastifyPlugin } from 'fastify-plugin'
 
+import { prisma } from '@/lib/prisma'
+
 import { UnauthorizedError } from '../routes/_errors/unauthorized-error'
 
 export const auth = fastifyPlugin(async (app: FastifyInstance) => {
@@ -12,6 +14,37 @@ export const auth = fastifyPlugin(async (app: FastifyInstance) => {
         return sub
       } catch {
         throw new UnauthorizedError('Invalid auth token')
+      }
+    }
+
+    request.getUserMembership = async (organizationSlug: string) => {
+      const userId = await request.getCurrentUserId()
+      const member = await prisma.member.findFirst({
+        where: {
+          userId,
+          organization: {
+            slug: organizationSlug,
+          },
+        },
+
+        // get the organization data
+        include: {
+          organization: true,
+        },
+      })
+
+      if (!member) {
+        throw new UnauthorizedError('You are not a member of the organization.')
+      }
+
+      const {
+        organization,
+        ...membership /* place all the other atributes in the variable membership */
+      } = member
+
+      return {
+        organization,
+        membership,
       }
     }
   })
