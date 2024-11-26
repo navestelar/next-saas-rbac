40fedcb1084312b9f7c1f825ad1b2f8fff8a1de2
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-11-25 22:20:34 -0300

    feat(front): github auth and logout

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,5 +1,27 @@
+import { getCookie } from 'cookies-next'
+import type { CookiesFn } from 'cookies-next/lib/types'
 import ky from 'ky'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  // interceptors
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookieStore: CookiesFn | undefined
+
+        // It is executing in server
+        if (typeof window === 'undefined') {
+          // lazy import
+          const { cookies: serverCookies } = await import('next/headers')
+          cookieStore = serverCookies
+        }
+
+        const token = getCookie('token', { cookies: cookieStore })
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
