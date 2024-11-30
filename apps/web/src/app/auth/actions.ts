61122540ddb136baa95f4e019df598b7d4290bba
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-11-30 10:20:07 -0300

    fix(front): env and theme switcher

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,14 +1,15 @@
 'use server'
 
 import { redirect } from 'next/navigation'
+import {env} from "@saas/env";
 
 export async function signInWithGithub() {
   const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubSignInURL.searchParams.set('client_id', 'Ov23liszZlsW4ddc0L4I')
+  githubSignInURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
   githubSignInURL.searchParams.set(
     'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
+    env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
   )
   githubSignInURL.searchParams.set('scope', 'user')
 
